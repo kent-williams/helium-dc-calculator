f1cb5ffceed5f99efea9f48a15388f4242b44a10
Kent Williams <k3nt00@gmail.com>
2020-03-12 16:52:11 -0700

    removed material ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import {TextField, Button} from '@material-ui/core';
+import DataCreditCalculator from './DataCreditCalculator';
 
 function App() {
   return (
     <div className="App">
-      <body className="App-body">
-        <Button variant="contained">Calculate</Button>
-        <form noValidate autoComplete="off">
-          <TextField id="number-of-devices" label="Number of Devices" variant="outlined" />
-          <TextField id="mb-per-device-per-day" label="MB of data/day/device" variant="outlined" />
-        </form>
-      </body>
+      <DataCreditCalculator />
     </div>
   );
 }
