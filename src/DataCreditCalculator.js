c421cf83cb3d1f05c5cb32dbc9a7208cda3f3c8d
Kent Williams <k3nt00@gmail.com>
2020-03-12 17:26:08 -0700

    basics in place

--- src/DataCreditCalculator.js
+++ src/DataCreditCalculator.js
@@ -3,96 +3,86 @@ import './DataCreditCalculator.css';
 
 export default class DataCreditCalculator extends Component {
     state = {
-        result: null,
-        fromCurrency: "USD",
-        toCurrency: "GBP",
-        amount: 1,
-        currencies: [],
+        costResult: null,
+        numberOfDevices: 100,
+        dataPerDevice: 1,
+        dataUnit: "MB",
+        timeUnit: "day",
+        dataUnits: ["KB", "MB", "GB"],
+        timeUnits: ["day", "week", "month", "year"],
     };
 
-    // Initializes the currencies with values from the Openrates API
+    // Initializes anything
     componentDidMount() {
-        // axios
-        //     .get("https://api.openrates.io/latest")
-        //     .then(response => {
-        //         // Initialized with 'EUR' because the base currency is 'EUR'
-        //         // and it is not included in the response
-        //         const currencyAr = ["EUR"]
-        //         for (const key in response.data.rates) {
-        //             currencyAr.push(key)
-        //         }
-        //         this.setState({ currencies: currencyAr.sort() })
-        //     })
-        //     .catch(err => {
-        //         console.log("Oops, something broke with GET in componentDidMount() - we've got a: ", err.message);
-        //     });
     }
+ 
+    calculateHandler = () => {
+        if (this.state.numberOfDevices > 0) {
 
-    // Event handler for the conversion BROKEN due to CORS policy
-    // https://q777nnrzpw.codesandbox.io/ WORKS here - 
-    convertHandler = () => {
-        // if (this.state.fromCurrency !== this.state.toCurrency) {
-        //     axios.get(`http://api.openrates.io/latest?base=${this.state.fromCurrency}&symbols=${this.state.toCurrency}`)
-        //         .then(response => {
-        //             const result = this.state.amount * (response.data.rates[this.state.toCurrency]);
-        //             this.setState({ result: result.toFixed(5) })
-        //         })
-        //         .catch(err => {
-        //             console.log("Oops, something broke with GET in convertHandler() - we've got a: ", err.message);
-        //         });
-        // } else {
-        //     this.setState({ result: "You can't convert the same currency!" })
-        // }
+            this.setState({ costResult: "$100" })
+
+        } else {
+            this.setState({ costResult: "Please enter a positive number of devices" })
+        }
     };
 
     // Updates the states based on the dropdown that was changed
     selectHandler = (event) => {
-        if (event.target.name === "from") {
-            this.setState({ fromCurrency: event.target.value })
+        if (event.target.name === "data unit") {
+            this.setState({ dataUnit: event.target.value })
         }
-        if (event.target.name === "to") {
-            this.setState({ toCurrency: event.target.value })
+        if (event.target.name === "time unit") {
+            this.setState({ timeUnit: event.target.value })
         }
     }
 
     render() {
         return (
             <div className="DataCreditCalculator">
-                <h2><span>Currency </span> Converter 
+                <h2><span>Data Credit Calculator </span> 
                 <div className="Form">
                     <input
-                        name="amount"
+                        name="number of devices"
                         type="text"
-                        value={this.state.amount}
+                        value={this.state.numberOfDevices}
+                        onChange={event =>
+                            this.setState({ amount: event.target.value })
+                        }>
+                    </input>
+
+                    <input
+                        name="data per device"
+                        type="text"
+                        value={this.state.dataPerDevice}
                         onChange={event =>
                             this.setState({ amount: event.target.value })
                         }>
                     </input>
 
                     <select
-                        name="from"
+                        name="data unit"
                         onChange={(event) => this.selectHandler(event)}
-                        value={this.state.fromCurrency}>
+                        value={this.state.dataUnit}>
 
-                        {this.state.currencies.map(cur => (
+                        {this.state.dataUnits.map(cur => (
                             <option key={cur}>{cur}</option>
                         ))}
                     </select>
 
                     <select
-                        name="to"
+                        name="time unit"
                         onChange={(event) => this.selectHandler(event)}
-                        value={this.state.toCurrency}>
+                        value={this.state.timeUnit}>
 
-                        {this.state.currencies.map(cur => (
+                        {this.state.timeUnits.map(cur => (
                             <option key={cur}>{cur}</option>
                         ))}
                     </select>
 
-                    <button onClick={this.convertHandler}>Convert</button>
+                    <button onClick={this.calculateHandler}>Calculate</button>
                 </div>
-                {this.state.result && 
-                    <h3>{this.state.result}</h3>
+                {this.state.costResult && 
+                    <h3>{this.state.costResult}</h3>
                 }
                 </h2>
             </div>
